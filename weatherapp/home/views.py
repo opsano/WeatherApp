a696e33111874a0233921f746c39944227390401
Nicolas Holmgren-Chinique <130803285+Nicolas-Holmgren-Chinique@users.noreply.github.com>
2025-12-18 19:57:00 -0800

    prep for the frontend and work on urls/views

--- weatherapp/home/views.py
+++ weatherapp/home/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+# simple function based view which will only render the front end
+def home(request):
+    return render(request, 'home/home.html')
