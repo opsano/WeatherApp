16e569d0e93a25c98aef57f7c10993d0a3506c78
opsano <greennates@gmail.com>
2025-12-20 18:36:07 -0800

    Cleaned repo, removed duplicate files, and untracked the database and cache files

--- weatherapp/home/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-
-# Create your views here.
-
-# simple function based view which will only render the front end
-def home(request):
-    return render(request, 'home/home.html')
